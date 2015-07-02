be2d14117841e437230b784d45bc40f27e4441fd
Saul Spatz <saul.spatz@gmail.com>
2015-07-02 13:41:28 -0500

    Initial Commit

---
+++ VizingShannon/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VizingShannon
+//
+//  Created by Saul Spatz on 7/2/15.
+//  Copyright (c) 2015 Saul Spatz. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
